894d67167314fa628fff846668842e0042328135
Jarrod Hasnain <jhasnain@uwo.ca>
2021-10-19 18:22:02 -0400

    Created Placeholder Player Asset

---
+++ Project Spectre/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    GameObject player; // Get the player object
+    float walkSpeed; // Character walking speed
+    float runSpeed; // Character running speed
+    float jumpForce; // Force applied when jumping
+    bool secondJump; // Does the player have a second jump
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
