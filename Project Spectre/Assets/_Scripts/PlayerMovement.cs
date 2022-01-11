e23849f0df511fa4fe04b8a594f2dce79df02b2d
Jarrod Hasnain <jhasnain@uwo.ca>
2022-01-11 18:11:43 -0500

    Updated Project Spectre

--- Project Spectre/Assets/_Scripts/PlayerMovement.cs
+++ Project Spectre/Assets/_Scripts/PlayerMovement.cs
@@ -5,16 +5,52 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    GameObject player; // Get the player object
-    float walkSpeed; // Character walking speed
-    float runSpeed; // Character running speed
-    float jumpForce; // Force applied when jumping
-    bool secondJump; // Does the player have a second jump
+    Rigidbody2D rb;
+    public float walkSpd;
+    public float jumpForce;
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Jump();
+    }
 
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * walkSpd;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 }
